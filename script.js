ddc22825a4395a85c7200017e494596936d1c28e
Rohit-Pandit <159272518+Rohit-Pandit@users.noreply.github.com>
2025-08-06 15:51:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
-
+	const s = str.replaceAll(' ','');
+	let i=0,j=str.length-1;
+	while(i<j){
+		
+		if((s[i].toLowerCase())!==(s[j].toLowerCase()))return false;
+		i++;
+		j--;
+	}
+return true;
 }
 module.exports = palindrome
